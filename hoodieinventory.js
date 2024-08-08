ca8657a2039ea1aee8daa43f33d5515eddb0c281
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 11:27:54 -0400

    Initial Commit

---
+++ hoodieinventory.js
@@ -0,0 +1,27 @@
+// Task 4: Prompt the user to enter their password and save it in a variable called passwordGuess
+
+
+Let Guesspassword = prompt("Enter your password")
+// Task 5: If passwordGuess is equal to the password, console log "Welcome [username]" and change status to "online". Else, console log "Wrong password" and change status to "offline"
+if(passwordGuess
+properties.password) {
+23
+console. log ( "welcome", properties .username)
+24
+properties.status = "online"
+25 ~ Jelse{
+26
+console. log ( "Wrong password" )
+27
+properties.status = "offline"
+
+}
+
+// Task 6: Create an object called hoodieInventory with the properties 'name' ("hoodie"), 'quantity' (any number), and 'price' (any number). 
+let hoodieinventory =
+  name:"designer",
+  quantity:"13",
+  price:"$400"
+;
+    // Task 7: Console log "[name] costs [price]"
+    console.log("[name", costs[price], ")");
---
+++ script.js
@@ -0,0 +1,15 @@
+// Task 1: Create variable called 'storeName' and set it to "CSH Store". Console log "Welcome to [storeName]"
+let storeName="csh store"
+console.log("Welcome to {storeName}")
+
+// Task 2: Create a user object with properties 'username' (any), 'password' (any), and 'status' (online or offline). 
+
+let object=
+  username:"beep_bop",
+password:"beep1928",
+ status:"Online"
+;
+// Task 3: Console log "[username] is [status]".
+
+console.log("[username] is {status}")
+
